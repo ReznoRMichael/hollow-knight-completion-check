4150e19e47c0525c3d8416eb41aed6026d01db96
ReznoRMichael <mic3arena@gmail.com>
2021-01-24 22:10:18 +0100

    include npm

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+    }
+};
