ccc1150bfb407501035b6f66367699aa28fa2fdc
ReznoRMichael <mic3arena@gmail.com>
2021-02-06 02:44:09 +0100

    cookie default options

--- src/js/cookieconsent.js
+++ src/js/cookieconsent.js
@@ -15,13 +15,57 @@ const cc = new CC({
     theme: "edgeless",
     type: "opt-in",
     content: {
-        message: "This website uses cookies for Google Analytics, for pure statistics purposes."
+        message: "This website uses cookies for Google Analytics, for pure statistics purposes.",
+        dismiss: 'Got it!',
+        allow: 'Allow cookies',
+        deny: 'Decline',
+        link: 'Learn more',
+        href: 'https://www.cookiesandyou.com',
+        close: '&#x274c;',
+        policy: 'Cookie Policy',
+        target: '_blank',
     }
 });
 
-cc.on("initialized", (...args) => console.log(args));
+// cc.on("initialized", (...args) => console.log(args));
+cc.on("initialized", (...args) => {
+    console.log(args);
+
+    let type = this.options.type;
+    let didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+        // enable cookies
+    }
+    if (type == 'opt-out' && !didConsent) {
+        // disable cookies
+    }
+});
+
+cc.on("revokeChoice", () => {
+    console.log("Popup Reset");
+
+    var type = this.options.type;
+    if (type == 'opt-in') {
+        // disable cookies
+    }
+    if (type == 'opt-out') {
+        // enable cookies
+    }
+});
+
+cc.on("statusChanged", (...args) => {
+    console.log(args);
+
+    let type = this.options.type;
+    let didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+        // enable cookies
+    }
+    if (type == 'opt-out' && !didConsent) {
+        // disable cookies
+    }
+});
+
 cc.on("error", console.error);
 cc.on("popupOpened", () => console.log("Popup Open"));
 cc.on("popupClosed", () => console.log("Popup Closed"));
-cc.on("revokeChoice", () => console.log("Popup Reset"));
-cc.on("statusChanged", (...args) => console.log(args));
