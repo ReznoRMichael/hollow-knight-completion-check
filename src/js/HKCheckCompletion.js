0de6a2e223a85839038c9afe44c39f4de7207431
ReznoRMichael <mic3arena@gmail.com>
2021-02-10 20:02:39 +0100

    emoji

--- src/js/HKCheckCompletion.js
+++ src/js/HKCheckCompletion.js
@@ -742,7 +742,7 @@ function CheckAdditionalThings(divId, dataObject, playerData, worldData, sceneDa
             for (let i = 0; i < arrayLength; i++) {
                 if (sceneData.geoRocks[i].hitsLeft > 0) {
                     countTotal++;
-                    console.log(`#${countTotal} | ${sceneData.geoRocks[i].id} | ${TranslateMapName(sceneData.geoRocks[i].sceneName)} | ${sceneData.geoRocks[i].sceneName}`);
+                    console.log(`#${countTotal} 🏔️ ${sceneData.geoRocks[i].id} 🗺️ ${TranslateMapName(sceneData.geoRocks[i].sceneName)} ⌨️ ${sceneData.geoRocks[i].sceneName}`);
                 }
             }
             console.groupEnd();
@@ -783,7 +783,7 @@ function CheckAdditionalThings(divId, dataObject, playerData, worldData, sceneDa
         if (length) {
             console.groupCollapsed("%cUnrescued Grubs List:", "color: #16c60c;");
             for (let i = 0; i < length; i++) {
-                console.log(`#${HK.GRUBS_LIST.indexOf(missingGrubsList[i]) + 1} | ${TranslateMapName(missingGrubsList[i])} | ${missingGrubsList[i]}`);
+                console.log(`#${HK.GRUBS_LIST.indexOf(missingGrubsList[i]) + 1} 🗺️ ${TranslateMapName(missingGrubsList[i])} ⌨️ ${missingGrubsList[i]}`);
             }
             console.groupEnd();
         }
