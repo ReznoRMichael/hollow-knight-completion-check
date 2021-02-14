50cdcfa6edb6b1c2e7df8fb6fd8a09260940088c
ReznoRMichael <mic3arena@gmail.com>
2021-02-14 18:16:43 +0100

    fade pantheons for backwards compatibility

--- src/js/HKCheckCompletion.js
+++ src/js/HKCheckCompletion.js
@@ -614,8 +614,14 @@ function CheckGodmasterDoors(divId, dataObject, playerData) {
     let pantheon = ["Master", "Artist", "Sage", "Knight"].map((element) => "pantheon" + element);
     
     for (let i = 0; i < 4; i++) {
-        (playerData["bossDoorStateTier" + (i + 1)].completed === true) ? CurrentDataTrue(divId): CurrentDataFalse();
-        FillHTML(divId, dataObject[pantheon[i]].name, dataObject[pantheon[i]].spoiler);
+        if (playerData.hasOwnProperty("bossDoorStateTier" + (i + 1)) === false) {
+            CurrentDataBlank();
+            FillHTML(divId, `<del>${dataObject[pantheon[i]].name}</del>`, `<del>${dataObject[pantheon[i]].spoiler}</del>`);
+        }
+        else {
+            (playerData["bossDoorStateTier" + (i + 1)].completed === true) ? CurrentDataTrue(divId): CurrentDataFalse();
+            FillHTML(divId, dataObject[pantheon[i]].name, dataObject[pantheon[i]].spoiler);
+        }
     }
 }
 
