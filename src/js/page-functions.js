578824f00add82b51ada351d64d5db3be027cbd2
ReznoRMichael <mic3arena@gmail.com>
2021-02-16 18:55:33 +0100

    scroll up button, visible class

--- src/js/page-functions.js
+++ src/js/page-functions.js
@@ -10,19 +10,20 @@ function ShowElement() {
 }
 
 function HideElement() {
-    
+
 }
 
 function DetectPageScroll() {
 
     /* Maximum number of pixels that can be scrolled by the user */
     let scrollTotal = ROOT.scrollHeight - ROOT.clientHeight;
+    let scrollUpButton = document.querySelector(".scroll-up-button");
 
-    if ((ROOT.scrollTop / scrollTotal ) > 0.80) {
-        //show button
-      } else {
-        //hide button
-      }
+    if ((ROOT.scrollTop / scrollTotal) > 0.80) {
+        /* Show button */
+    } else {
+        /* Hide button */
+    }
 }
 
 document.addEventListener("scroll", DetectPageScroll);
