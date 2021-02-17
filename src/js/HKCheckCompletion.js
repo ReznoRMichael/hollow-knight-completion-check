4c5b654677a0cf82ab0de0a410691a60224a2335
ReznoRMichael <mic3arena@gmail.com>
2021-02-17 17:54:30 +0100

    formatting

--- src/js/HKCheckCompletion.js
+++ src/js/HKCheckCompletion.js
@@ -603,13 +603,20 @@ function CheckGodmasterDoors(divId, dataObject, playerData) {
         // compatibility with earlier game versions
         if (playerData.hasOwnProperty("bossDoorStateTier" + (i + 1)) === false) {
             CurrentDataBlank();
-            sFillText += PrepareHTMLString(divId,
+            sFillText += PrepareHTMLString(
+                divId,
                 `<del>${dataObject[pantheon[i]].name}</del>`,
                 `<del>${dataObject[pantheon[i]].spoiler}</del>`,
-                `<del>${dataObject[pantheon[i]].wiki}</del>`);
+                `<del>${dataObject[pantheon[i]].wiki}</del>`
+                );
         } else {
             (playerData["bossDoorStateTier" + (i + 1)].completed === true) ? CurrentDataTrue(divId): CurrentDataFalse();
-            sFillText += PrepareHTMLString(divId, dataObject[pantheon[i]].name, dataObject[pantheon[i]].spoiler, dataObject[pantheon[i]].wiki);
+            sFillText += PrepareHTMLString(
+                divId,
+                dataObject[pantheon[i]].name,
+                dataObject[pantheon[i]].spoiler,
+                dataObject[pantheon[i]].wiki
+                );
         }
     }
 
