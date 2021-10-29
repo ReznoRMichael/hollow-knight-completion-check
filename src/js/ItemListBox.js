24ef767f48657eef9f2370018164c78ab1a72f6d
ReznoRMichael <mic3arena@gmail.com>
2021-10-29 11:50:27 +0200

    class ItemListBox

--- src/js/ItemListBox.js
+++
@@ -1,5 +0,0 @@
-class ItemListBox {
-  constructor() {
-    
-  }
-}
---
+++ src/js/page.js
@@ -0,0 +1,6 @@
+/* This file will contain all classes and functions for creating things on the page */
+class ItemListBox {
+  constructor() {
+    console.log("ItemListBox is being created");
+  }
+}
