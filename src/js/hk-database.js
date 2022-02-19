1e22418d19a7539d495c50eea92e14a6fe1b8ec1
ReznoRMichael <mic3arena@gmail.com>
2022-02-19 20:01:26 +0100

    add The Abyss Door Opened to database

--- src/js/hk-database.js
+++ src/js/hk-database.js
@@ -1299,14 +1299,16 @@ const HK = {
           spoiler: "City of Tears, requires Love Key",
           wiki: "City_of_Tears#Sub-area:_Tower_of_Love"
         },
+        abyssGateOpened: {
+          name: "The Abyss Door Opened",
+          spoiler: "Ancient Basin, requires King's Brand",
+          wiki: "Ancient_Basin#Description"
+        },
         blueVineDoor: {
           name: "Lifeblood Door Opened",
           spoiler: "The Abyss, requires 14–15+ Lifeblood Masks",
           wiki: "Lifeblood_Core#How_to_Acquire"
         },
-        /* 
-        "abyssGateOpened": false,
-        */
         hasTramPass: {
           name: "Tram Pass",
           spoiler: "Deepnest: Failed Tramway",
